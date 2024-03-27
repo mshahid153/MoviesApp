4e9893e4053dd88b15ef2eaea7004a34289ffaf9
root <mdshahid150303@gmail.com>
2024-03-27 13:57:45 +0000

    Initial Commit

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import {v4 as uuidv4} from 'uuid'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route key={uuidv4()} {...props} />
+}
+
+export default ProtectedRoute
